6f32ccd2b1afcc18303684621c3c73871d368850
adamrees89 <adamrees89@googlemail.com>
2023-02-14 10:15:43 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Update All Packages
+This is a simple script that enables a one-click update/upgrade of all python packages using PUR. https://pypi.org/project/pur/
+
+# To-do
+
+- [ ] Add a try/except loop around package install to silently catch errors and move on
---
+++ UpdateAllPackages.py
@@ -0,0 +1,22 @@
+import subprocess
+import sys
+import os
+
+# Check if pur is installed, and warn user if it's not
+try:
+    subprocess.check_call([sys.executable, '-m', 'pip', 'show', 'pur'])
+except subprocess.CalledProcessError:
+    print('WARNING: pur is not installed. Please run "pip install pur" to install it.')
+    sys.exit(1)
+
+# Generate requirements file from current environment
+subprocess.call(['pip', 'freeze', '--local', '>', 'requirements.txt'])
+
+# Use pur to update requirements file
+subprocess.call(['pur', '-r', 'requirements.txt'])
+
+# Use pip to update all packages
+subprocess.call(['pip', 'install', '-r', 'requirements.txt', '--upgrade'])
+
+# Clean up the requirements file
+os.remove('requirements.txt')
